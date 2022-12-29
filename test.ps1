28de2d3e9a96ba7fa3d0c2bb25008b4723b88010
biostar1020 <121612826+biostar1020@users.noreply.github.com>
2022-12-29 14:58:55 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 $outlook = new-object -comobject outlook.application
 
 $email = $outlook.CreateItem(0)
-$email.To = biostar1020@gmail.com
+$email.To = "biostar1020@gmail.com"
 $email.Subject = "New email test"
 $email.Body = "This is a testing email"
 $email.Attachments.add("C:\Users\talharthi\Desktop\0.txt")
