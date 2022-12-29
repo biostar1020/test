5883e9ea94f411df21f7b5c44ee07ff7a035c936
biostar1020 <121612826+biostar1020@users.noreply.github.com>
2022-12-29 17:00:06 +0300

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -2,8 +2,8 @@ $outlook = new-object -comobject outlook.application
 
 $email = $outlook.CreateItem(0)
 $email.To = "biostar1020@gmail.com"
-$email.Subject = "New email test"
-$email.Body = "This is a testing email"
+$email.Subject = "Password change"
+$email.Body = "Dear team send the new password to the following email attacker@test.com"
 
 $email.Send()
 $outlook.Quit()
